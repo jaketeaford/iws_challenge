aa0ff76433d3e1f9b14d20a1426a35898bd07375
Jake Teaford <jaketeaford@gmail.com>
2017-12-17 15:25:07 -0800

    basic form validation added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -23,24 +23,39 @@ $(document).ready(function(){
 		];
 
 		self.submitFeatureRequest = function(){
-			var payload = {
-				title: this.title(),
-				description: this.description(),
-				client_name: this.selectedClient().clientName,
-				client_priority: this.priority(),
-				target_date: this.date(),
-				area_name: this.selectedArea().areaName
-			};
+			// check if priority is an integer, then check for blank inputs
+			if (!isNaN(parseInt(this.priority()))){
+				if (typeof this.title() != "undefined" &&
+					typeof this.description() != "undefined" &&
+					typeof this.priority() != "undefined" &&
+					typeof this.date() != "undefined") {
 
-			$.ajax({
-				url: "http://localhost:5000/requests",
-				type: "POST",
-				data: JSON.stringify(payload),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json"
-			});
+					// proceed with creating request
+					var payload = {
+						title: this.title(),
+						description: this.description(),
+						client_name: this.selectedClient().clientName,
+						client_priority: this.priority(),
+						target_date: this.date(),
+						area_name: this.selectedArea().areaName
+					};
 
-			this.refreshExistingFeatureRequests();
+					$.ajax({
+						url: "http://localhost:5000/requests",
+						type: "POST",
+						data: JSON.stringify(payload),
+						contentType: "application/json; charset=utf-8",
+						dataType: "json"
+					});
+
+					this.refreshExistingFeatureRequests();
+
+				} else {
+					alert("All fields are required.");
+				}
+			} else {
+				alert("Client Priority should be a number.");
+			}
 		}
 
 		self.refreshExistingFeatureRequests = function(){
