c692bc912d2c64309b7e9e819c0475caf377cff6
Jake Teaford <jaketeaford@gmail.com>
2017-12-17 12:37:29 -0800

    can now create database entries from the frontend form

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import jsonify
+from flask import jsonify, request
 from database import app, db, FeatureRequest
+from dateutil.parser import parse
+import json
 
 #mysql root pw is 07g1fM*DC834
 
@@ -7,14 +9,39 @@ from database import app, db, FeatureRequest
 def index():
 	return app.send_static_file("index.html")
 
-@app.route("/requests")
+@app.route("/requests", methods=['GET', 'POST'])
 def requests():
-	requests = FeatureRequest.query.all()
-	result = []
-	for request in requests:
-		request_dict = dict(
-			id=request.id,
-			name=request.title
+	if request.method == 'POST':
+		# create request
+		data = request.get_json()
+
+		mysql_date = parse(data['target_date'])
+
+		featRequest = FeatureRequest(
+			title = data['title'],
+			description = data['description'],
+			client_name = data['client_name'],
+			client_priority = data['client_priority'],
+			target_date = mysql_date,
+			area_name = data['area_name']
 		)
-		result.append(request_dict)
-	return jsonify(result)
+
+		db.session.add(featRequest)
+		db.session.commit()
+
+		return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+	else: 
+		# retrieve feature requests
+		feature_requests = FeatureRequest.query.all()
+		result = []
+		for feature_request in feature_requests:
+			request_dict = dict(
+				title=feature_request.title,
+				description=feature_request.description,
+				client_name=feature_request.client_name,
+				client_priority=feature_request.client_priority,
+				target_date=feature_request.target_date,
+				area_name=feature_request.area_name,
+			)
+			result.append(request_dict)
+		return jsonify(result)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	    self.priority = ko.observable();
 	    self.date = ko.observable();
 	    self.selectedArea = ko.observable();
-
+	    self.existingRequests = ko.observableArray([]);
 
 	    self.clients = [
 	    	{ clientName: "Client A" },
@@ -22,9 +22,31 @@ $(document).ready(function(){
 	    	{ areaName: "Reports" }
 		];
 
-		self.submitFeatureRequest = function(){};
-	}
-	 
+		self.submitFeatureRequest = function(){
+			var payload = {
+				title: this.title(),
+				description: this.description(),
+				client_name: this.selectedClient().clientName,
+				client_priority: this.priority(),
+				target_date: this.date(),
+				area_name: this.selectedArea().areaName
+			};
+
+			$.ajax({
+				url: "/requests",
+				type: "POST",
+				data: JSON.stringify(payload),
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				success: function(){
+
+				}
+			});
+		}
+
+		self.refreshExistingFeatureRequests = function(){}
+ 	}
+	
 	$('[data-toggle="datepicker"]').datepicker();
 
 	ko.applyBindings(new AppViewModel());
